d3f38c5c074288a4ffd431242f1b5551bcd2adf7
marciotojr <marciotojr@gmail.com>
2016-02-03 19:51:25 -0200

    Telas de adição de Professor, Salas e Turmas

--- listaProfessorGrid.php
+++ listaProfessorGrid.php
@@ -15,58 +15,57 @@ include("header.php");
 
 <div class="nova-grade-block">
 
-        <fieldset>
-            <legend>Polos</legend>
-            <fieldset class="form-group">
-                <table class="strippedHover">
-                    <tr><th>A&ccedil;&otilde;es</th><th>Polo</th><th>Cidade</th></tr>
-                    <?php
-                    $id = "";
-                    $uf = "";
-                    $nome = "";
-                    $cidade = "";
-                    $polo = "";
+    <fieldset>
+        <legend>Professores</legend>
+        <fieldset class="form-group">
+            <table class="strippedHover">
+                <tr><th>A&ccedil;&otilde;es</th><th>Nome</th><th>Departamento</th><th>Polo</th></tr>
+                <?php
+                $id = "";
+                $codigo = "";
+                $nome = "";
+                $departamento = "";
+                $polo = "";
 
-                    $conn = mysqli_connect('localhost', 'root', '', 'ihc1');
-                    if (!$conn)
-                      die("Erro fatal. Não foi possível se conectar ao banco de dados.");
+                $conn = mysqli_connect('localhost', 'root', '', 'ihc1');
+                if (!$conn)
+                  die("Erro fatal. Não foi possível se conectar ao banco de dados.");
 //$db = mysqli_select_db('ihc1', $link) or die('Could not select database.');
 
-                    $sql = "SELECT id as id, nome as polo, cidade as cidade, uf as uf FROM polo ORDER BY polo";
-                    $result = mysqli_query($conn, $sql);
+                $sql = "SELECT p.id as id, p.nome as nome, d.nome as departamento, po.nome as polo FROM professor p, departamento d, polo po, departamento_polo dp WHERE p.departamento=dp.id and dp.id_polo = po.id and dp.id_departamento=d.id ORDER BY polo, departamento, nome";
+                $result = mysqli_query($conn, $sql);
 
-                    if (mysqli_num_rows($result) > 0) {
-                      while ($disciplina = mysqli_fetch_assoc($result)) {
-                        extract($disciplina, EXTR_OVERWRITE);
-                        echo '<tr><td><a href="gerenciarPoloGrid.php?id=' . $id . '"><span class="badge badge-edit"><i class="glyphicon glyphicon-pencil"></i></span></a>'
-                        . '<a href="#" onclick="excluir()"><span class="badge badge-important"><i class="glyphicon glyphicon-remove"></i></span></a></td>';
-                        echo '<td>' . $polo . '</td><td>' . $cidade . ' / ' . $uf . '</td></tr>';
-                      }
-                    } else {
-                      echo "<tr><td colspan=5>Nenhuma disciplina registrada.</td></tr>";
-                    }
-                    ?>
-                </table>
+                if (mysqli_num_rows($result) > 0) {
+                  while ($professor = mysqli_fetch_assoc($result)) {
+                    extract($professor, EXTR_OVERWRITE);
+                    echo '<tr><td><a href="#" onclick="changeContent(\'gerenciarProfessor.php?id=' . $id . '\')"><span class="badge badge-edit"><i class="glyphicon glyphicon-pencil"></i></span></a>'
+                    . '<a href="#" onclick="changeContent(\'excluirProfessor.php?id=' . $id . '\')"><span class="badge badge-important"><i class="glyphicon glyphicon-remove"></i></span></a></td>';
+                    echo '<td>' . $nome . '</td><td>' . $departamento . '</td><td>' . $polo . '</td></tr>';
+                  }
+                } else {
+                  echo "<tr><td colspan=5>Nenhuma disciplina registrada.</td></tr>";
+                }
+                ?>
+            </table>
+        </fieldset>
 
-            </fieldset>
-  
-    <fieldset>
-        <button type="submit" class="btn btn-primary" onclick="redirecionar(0)">Inserir</button>
-        <button type="submit" class="btn btn-primary" onClick="redirecionar(1)">Voltar</button>
-    </fieldset>
-    <script type="text/javascript" language="javascript" charset="utf-8">
-      function redirecionar(opt)
-      {
-            if (opt == 0)
-            {
-                window.location = "gerenciarPoloGrid.php";
-            }
-            else if (opt == 1)
-            {
-                window.location = "principal.php";
-            }
-      }
-    </script>
+        <fieldset>
+            <button type="submit" class="btn btn-primary" onclick="redirecionar(0)">Inserir</button>
+            <button type="submit" class="btn btn-primary" onClick="redirecionar(1)">Voltar</button>
+        </fieldset>
+        <script type="text/javascript" language="javascript" charset="utf-8">
+          function redirecionar(opt)
+          {
+              if (opt == 0)
+              {
+                  window.location = "gerenciarProfessorGrid.php";
+              }
+              else if (opt == 1)
+              {
+                  window.location = "principal.php";
+              }
+          }
+        </script>
 </div>
 <script>
   function excluir()
--- listaTurmasGrid.php
+++ listaTurmasGrid.php
@@ -15,58 +15,57 @@ include("header.php");
 
 <div class="nova-grade-block">
 
-        <fieldset>
-            <legend>Polos</legend>
-            <fieldset class="form-group">
-                <table class="strippedHover">
-                    <tr><th>A&ccedil;&otilde;es</th><th>Polo</th><th>Cidade</th></tr>
-                    <?php
-                    $id = "";
-                    $uf = "";
-                    $nome = "";
-                    $cidade = "";
-                    $polo = "";
+    <fieldset>
+        <legend>Turmas</legend>
+        <fieldset class="form-group">
+            <table class="strippedHover">
+                <tr><th>A&ccedil;&otilde;es</th><th>Codigo</th><th>Turma</th><th>Disciplina</th><th>Departamento</th><th>Polo</th></tr>
+                <?php
+                $id = "";
+                $codigo = "";
+                $nome = "";
+                $departamento = "";
+                $polo = "";
 
-                    $conn = mysqli_connect('localhost', 'root', '', 'ihc1');
-                    if (!$conn)
-                      die("Erro fatal. Não foi possível se conectar ao banco de dados.");
+                $conn = mysqli_connect('localhost', 'root', '', 'ihc1');
+                if (!$conn)
+                  die("Erro fatal. Não foi possível se conectar ao banco de dados.");
 //$db = mysqli_select_db('ihc1', $link) or die('Could not select database.');
 
-                    $sql = "SELECT id as id, nome as polo, cidade as cidade, uf as uf FROM polo ORDER BY polo";
-                    $result = mysqli_query($conn, $sql);
+                $sql = "SELECT t.id as id, d.codigo as codigo, d.nome as nome, de.nome as departamento, p.nome as polo, t.turma as turma FROM disciplina d, departamento_polo dp, departamento de, polo p, turma t WHERE d.id_departamento_polo=dp.id and dp.id_departamento=de.id and dp.id_polo = p.id and t.id_disciplina=d.id ORDER BY polo, departamento, codigo, turma";
+                $result = mysqli_query($conn, $sql);
 
-                    if (mysqli_num_rows($result) > 0) {
-                      while ($disciplina = mysqli_fetch_assoc($result)) {
-                        extract($disciplina, EXTR_OVERWRITE);
-                        echo '<tr><td><a href="gerenciarPoloGrid.php?id=' . $id . '"><span class="badge badge-edit"><i class="glyphicon glyphicon-pencil"></i></span></a>'
-                        . '<a href="#" onclick="excluir()"><span class="badge badge-important"><i class="glyphicon glyphicon-remove"></i></span></a></td>';
-                        echo '<td>' . $polo . '</td><td>' . $cidade . ' / ' . $uf . '</td></tr>';
-                      }
-                    } else {
-                      echo "<tr><td colspan=5>Nenhuma disciplina registrada.</td></tr>";
-                    }
-                    ?>
-                </table>
+                if (mysqli_num_rows($result) > 0) {
+                  while ($disciplina = mysqli_fetch_assoc($result)) {
+                    extract($disciplina, EXTR_OVERWRITE);
+                    echo '<tr><td><a href="#" onclick="changeContent(\'gerenciarTurma.php?id=' . $id . '\')"><span class="badge badge-edit"><i class="glyphicon glyphicon-pencil"></i></span></a>'
+                    . '<a href="#" onclick="changeContent(\'excluirTurma.php?id=' . $id . '\')"><span class="badge badge-important"><i class="glyphicon glyphicon-remove"></i></span></a></td>';
+                    echo '<td>' . $codigo . '</td><td>' . $turma . '</td><td>' . $nome . '</td><td>' . $departamento . '</td><td>' . $polo . '</td></tr>';
+                  }
+                } else {
+                  echo "<tr><td colspan=5>Nenhuma disciplina registrada.</td></tr>";
+                }
+                ?>
+            </table>
+        </fieldset>
 
-            </fieldset>
-  
-    <fieldset>
-        <button type="submit" class="btn btn-primary" onclick="redirecionar(0)">Inserir</button>
-        <button type="submit" class="btn btn-primary" onClick="redirecionar(1)">Voltar</button>
-    </fieldset>
-    <script type="text/javascript" language="javascript" charset="utf-8">
-      function redirecionar(opt)
-      {
-            if (opt == 0)
-            {
-                window.location = "gerenciarPoloGrid.php";
-            }
-            else if (opt == 1)
-            {
-                window.location = "principal.php";
-            }
-      }
-    </script>
+        <fieldset>
+            <button type="submit" class="btn btn-primary" onclick="redirecionar(0)">Inserir</button>
+            <button type="submit" class="btn btn-primary" onClick="redirecionar(1)">Voltar</button>
+        </fieldset>
+        <script type="text/javascript" language="javascript" charset="utf-8">
+          function redirecionar(opt)
+          {
+              if (opt == 0)
+              {
+                  window.location = "gerenciarTurmaGrid.php";
+              }
+              else if (opt == 1)
+              {
+                  window.location = "principal.php";
+              }
+          }
+        </script>
 </div>
 <script>
   function excluir()
